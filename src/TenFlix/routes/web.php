80e9a76f7f8d0687dfcd2650048a8e5e4ffabc92
seala2005 <sean.larsen0408@gmail.com>
2025-11-18 15:42:02 +0100

    Made small tweaks for bind mount and routing

--- src/TenFlix/routes/web.php
+++ src/TenFlix/routes/web.php
@@ -8,13 +8,11 @@ Route::get('/', function () {
     return view('pages.home', ['movies' => $movies]);
 });
 
-Route::get('/index.html', function () {
-    $movies = Movie::all();
-    return view('pages.home', ['movies' => $movies]);
-});
 
 Route::view('/login.html', 'pages.login');
+
 Route::view('/signup.html', 'pages.signup');
+
 Route::get('/topten_slider.html', function () {
 	$movies = App\Models\Movie::orderBy('vote_count', 'desc')->take(10)->get();
 	return view('pages.topten_slider', ['movies' => $movies]);
