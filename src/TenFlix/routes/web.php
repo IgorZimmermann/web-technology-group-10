b5a35be2df2528d3b0e3ffbd14fea3828b4373d0
seala2005 <sean.larsen0408@gmail.com>
2025-11-19 10:53:50 +0100

    added crime and thriller section

--- src/TenFlix/routes/web.php
+++ src/TenFlix/routes/web.php
@@ -10,10 +10,20 @@ Route::get('/', function () {
         ->orderBy('vote_count', 'desc')
         ->take(12)
         ->get();
+    $thrillerMovies = Movie::where('genre', 'like', '%Thriller%')
+        ->orderBy('vote_count', 'desc')
+        ->take(12)
+        ->get();
+    $crimeMovies = Movie::where('genre', 'like', '%Crime%')
+        ->orderBy('vote_count', 'desc')
+        ->take(12)
+        ->get();
     return view('pages.home', [
         'movies' => $movies,
         'topMovies' => $topMovies,
         'actionMovies' => $actionMovies,
+        'thrillerMovies' => $thrillerMovies,
+        'crimeMovies' => $crimeMovies,
     ]);
 });
 
