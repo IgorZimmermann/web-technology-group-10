4f13902e090c0f313fc7a1f910c6900bbd05c05e
denis-nedret <denis.nedret0@gmail.com>
2025-11-20 14:56:28 +0100

    tmdb_id is now nullable and its unique index is removed (this will allow custom movies to be added)

---
+++ src/TenFlix/app/Http/Controllers/MovieController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class MovieController extends Controller
+{
+   public function index()
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+
+    public function reset()
+    {
+
+    }
+}
+
+
+
+ 
